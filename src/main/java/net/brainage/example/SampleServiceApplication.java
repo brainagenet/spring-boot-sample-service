f54d28cc5523b1fe00a948545c36f139096bf282
brainagenet <ms29.seo@gmail.com>
2018-08-10 02:27:30 +0900

    initial import

---
+++ src/main/java/net/brainage/example/SampleServiceApplication.java
@@ -0,0 +1,12 @@
+package net.brainage.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleServiceApplication.class, args);
+    }
+}
